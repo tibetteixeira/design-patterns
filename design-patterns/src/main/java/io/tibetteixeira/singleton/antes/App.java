85e6f88e9e58e2e1c9f6015c87f83bd87b45e5c7
Tibet Teixeira <tibetbrasil@gmail.com>
2024-04-20 23:27:02 -0300

    Implementação do singleton - antes

---
+++ design-patterns/src/main/java/io/tibetteixeira/singleton/antes/App.java
@@ -0,0 +1,9 @@
+package io.tibetteixeira.singleton.antes;
+
+public class App {
+    public static void main(String[] args) {
+        Cliente.buscarTodos();
+        Cliente.buscarInadimplentes();
+        Cliente.buscarVips();
+    }
+}
