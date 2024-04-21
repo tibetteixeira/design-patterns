d715a88e24ca466bf6cb2c211e04eb64f6395c15
Tibet Teixeira <tibetbrasil@gmail.com>
2024-04-20 23:27:30 -0300

    Implementação do singleton - depois

---
+++ design-patterns/src/main/java/io/tibetteixeira/singleton/depois/App.java
@@ -0,0 +1,9 @@
+package io.tibetteixeira.singleton.depois;
+
+public class App {
+    public static void main(String[] args) {
+        Cliente.buscarTodos();
+        Cliente.buscarInadimplentes();
+        Cliente.buscarVips();
+    }
+}
---
+++ design-patterns/src/main/java/io/tibetteixeira/singleton/depois/connection/Connection.java
@@ -0,0 +1,15 @@
+package io.tibetteixeira.singleton.depois.connection;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Connection {
+
+    private boolean disponivel = true;
+
+    public void query(String query) {
+        System.out.println(String.format("Executando a query `%s`", query));
+    }
+}
