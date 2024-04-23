64e40888c6d5b534419d2b22b7907f0c12739e04
Tibet Teixeira <tibetbrasil@gmail.com>
2024-04-22 23:18:01 -0300

    Implementação do prototype clonable

---
+++ design-patterns/src/main/java/io/tibetteixeira/prototype/clonable/Documento.java
@@ -0,0 +1,26 @@
+package io.tibetteixeira.prototype.clonable;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@ToString(includeFieldNames = false)
+public class Documento {
+
+    private String nome;
+    private String tipo;
+    private String visibilidade;
+    private String conteudo;
+
+    public Documento clone()  {
+        return new Documento(nome, tipo, visibilidade, conteudo);
+    }
+
+    public void imprimir() {
+        System.out.println(this);
+    }
+}
