442a9169b9f642a10f4ecd00937272e00a5c0979
Tibet Teixeira <tibetbrasil@gmail.com>
2024-05-10 08:06:16 -0300

    builder

---
+++ design-patterns/src/main/java/io/tibetteixeira/builder/depois/FastFoodMeal.java
@@ -0,0 +1,17 @@
+package io.tibetteixeira.builder.depois;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class FastFoodMeal {
+    private String drink;
+    private String main;
+    private String side;
+    private String dessert;
+    private String gift;
+
+}
